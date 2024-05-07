81e4c36e56d804fe7c3f253a0fbd919b91bb91e0
FGJCofficial <149368711+FGJC04@users.noreply.github.com>
2024-05-07 14:02:07 +0800

    Add files via upload

---
+++ displayNotecode.js
@@ -0,0 +1,164 @@
+
+
+let notes = document.querySelectorAll("#note");
+let txt = document.querySelector("#txt");
+let h1 = document.querySelector("h1");
+
+// close all excixting opend in .js
+h1.addEventListener("click",() => {
+    document.querySelector("#code0").style.display = "none";
+    document.querySelector("#code1").style.display = "none";
+    document.querySelector("#code2").style.display = "none";
+    document.querySelector("#code3").style.display = "none";
+    document.querySelector("#code4").style.display = "none";
+    document.querySelector("#code5").style.display = "none";
+    document.querySelector("#code6").style.display = "none";
+    document.querySelector("#code7").style.display = "none";
+    document.querySelector("#code8").style.display = "none";
+    document.querySelector("#code9").style.display = "none";
+    document.querySelector("#code10").style.display = "none";
+    document.querySelector("#code11").style.display = "none";
+    document.querySelector("#code12").style.display = "none";
+    document.querySelector("#code13").style.display = "none";
+    document.querySelector("#code14").style.display = "none";
+    document.querySelector("#code15").style.display = "none";
+    document.querySelector("#code16").style.display = "none";
+    document.querySelector("#code17").style.display = "none";
+    document.querySelector("#code18").style.display = "none";
+    document.querySelector("#code19").style.display = "none";
+    document.querySelector("#code20").style.display = "none";
+    document.querySelector("#code21").style.display = "none";
+    document.querySelector("#code22").style.display = "none";
+    document.querySelector("#code23").style.display = "none";
+    document.querySelector("#code24").style.display = "none";
+    txt.classList.toggle("txttoggle");
+});
+
+
+notes[0].addEventListener("click",() =>{
+    let style = document.querySelector("#code0");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[1].addEventListener("click",() =>{
+    let style = document.querySelector("#code1");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[2].addEventListener("click",() =>{
+    let style = document.querySelector("#code2");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[3].addEventListener("click",() =>{
+    let style = document.querySelector("#code3");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[4].addEventListener("click",() =>{
+    let style = document.querySelector("#code4");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[5].addEventListener("click",() =>{
+    let style = document.querySelector("#code5");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[6].addEventListener("click",() =>{
+    let style = document.querySelector("#code6");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[7].addEventListener("click",() =>{
+    let style = document.querySelector("#code7");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[8].addEventListener("click",() =>{
+    let style = document.querySelector("#code8");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[9].addEventListener("click",() =>{
+    let style = document.querySelector("#code9");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[10].addEventListener("click",() =>{
+    let style = document.querySelector("#code10");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[11].addEventListener("click",() =>{
+    let style = document.querySelector("#code11");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[12].addEventListener("click",() =>{
+    let style = document.querySelector("#code12");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[13].addEventListener("click",() =>{
+    let style = document.querySelector("#code13");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[14].addEventListener("click",() =>{
+    let style = document.querySelector("#code14");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[15].addEventListener("click",() =>{
+    let style = document.querySelector("#code15");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[16].addEventListener("click",() =>{
+    let style = document.querySelector("#code16");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[17].addEventListener("click",() =>{
+    let style = document.querySelector("#code17");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[18].addEventListener("click",() =>{
+    let style = document.querySelector("#code18");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[19].addEventListener("click",() =>{
+    let style = document.querySelector("#code19");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[20].addEventListener("click",() =>{
+    let style = document.querySelector("#code20");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[21].addEventListener("click",() =>{
+    let style = document.querySelector("#code21");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[22].addEventListener("click",() =>{
+    let style = document.querySelector("#code22");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[23].addEventListener("click",() =>{
+    let style = document.querySelector("#code23");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+notes[24].addEventListener("click",() =>{
+    let style = document.querySelector("#code24");
+    txt.classList.toggle("txttoggle");
+    style.style.display = "block";
+});
+
+
